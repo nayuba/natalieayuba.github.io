a73d225e709234f80684be573f1eea1c2766cb4d
Natalie Ayuba <natalieayuba@hotmail.com>
2023-04-07 23:54:32 +0100

    edited config

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  basePath: "/nextjs-page/",
-  assetPrefix: "/nextjs-page/",
+  basePath: "/nextjs-page",
+  assetPrefix: "/nextjs-page",
 };
 
 module.exports = nextConfig;
