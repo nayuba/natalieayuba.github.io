04bc37173f10edcfd72138c1b153debeed638f8c
Natalie Ayuba <natalieayuba@hotmail.com>
2023-04-07 23:35:20 +0100

    edited config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    loader: "akamai",
+    path: "",
   },
+  basePath: "/nextjs-page",
+  assetPrefix: "/nextjs-page",
 };
 
 module.exports = nextConfig;
